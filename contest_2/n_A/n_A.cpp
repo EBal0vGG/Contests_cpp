09db7cea1005181c8dda9723063c7618d15677f7
EBal0vGG <balovg2006@yandex.ru>
2025-01-30 17:07:00 +0300

    Added solution of contest-2 num-1

---
+++ contest_2/n_A/n_A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+
+class TimerGuard {
+public:
+    TimerGuard(const std::string& mes, std::ostream& out) :
+        mes_(mes),
+        out_(out),
+        time_1(std::chrono::high_resolution_clock::now())
+    {}
+
+    ~TimerGuard() {
+        auto time_2 = std::chrono::high_resolution_clock::now();
+        auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(time_2 - time_1);
+        out_ << mes_ << " " << dur.count() << std::endl;
+    }
+
+private:
+    std::string mes_;
+    std::ostream& out_;
+    std::chrono::time_point<std::chrono::high_resolution_clock> time_1;
+};
